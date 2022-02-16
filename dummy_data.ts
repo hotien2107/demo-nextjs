d8065e6fda7d154277be4d36cf82be299fe05eb7
Ho Tien <hotien2107@gmail.com>
2022-02-16 15:43:32 +0700

    code demo

---
+++ dummy_data.ts
@@ -0,0 +1,14 @@
+export const memberList = [
+  {
+    id: "1",
+    name: "Ngoc",
+  },
+  {
+    id: "2",
+    name: "Ly",
+  },
+  {
+    id: "3",
+    name: "Tien",
+  },
+];
---
+++ pages/team/[memberId].tsx
@@ -0,0 +1,28 @@
+import Head from "next/head";
+import { useRouter } from "next/router";
+import { memberList } from "../../dummy_data";
+
+const Tien = () => {
+  const router = useRouter();
+  const { memberId } = router.query;
+
+  const member = memberList.find((member) => member.id === memberId);
+
+  return (
+    <>
+      <Head>
+        <title>Tien</title>
+      </Head>
+      <button
+        onClick={() => {
+          router.push("/");
+        }}
+      >
+        Home
+      </button>
+      <h1>{member?.name}</h1>
+    </>
+  );
+};
+
+export default Tien;
